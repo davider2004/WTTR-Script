b86b647d3d87930ee17fa642c1ecd0f31b9df130
DAVIDE RAMONDETTI <info@ramondettidavide.com>
2017-01-27 16:35:05 +0100

    Update wttr.sh

--- wttr.sh
+++ wttr.sh
@@ -5,3 +5,16 @@
 
 CITY="Turin" #NAME_OF_YOUR_SITE_IN_ENGLISH
 
+figlet "WTTR-Script"
+echo "Made by RamondettiDavide.com"
+echo "=============================================================="
+echo
+echo "Opening WTTR.IN..."
+echo
+
+WEB="http://wttr.in/"$CITY
+
+curl $WEB
+
+echo
+echo "Arrivederci!"
