928fe251400c44b3cf9fddcc1f58d5dc779306b8
DAVIDE RAMONDETTI <info@ramondettidavide.com>
2017-01-27 16:05:46 +0100

    Create wttr-install.sh

---
+++ wttr-install.sh
@@ -0,0 +1,27 @@
+#######################################
+# (c) 2017 RamondettiDavide Softwares #
+#          ramondettidavide.com       #
+#######################################
+
+sudo apt-get update
+sudo apt-get install figlet -y
+
+clear
+
+figlet "InfornBlog"
+echo "===================================================="
+
+echo "PRONTO!"
+echo 
+echo "This installer was made and distributed by RamondettiDavide.com. Do not download this software from unofficial sources."
+echo
+echo "Packs to install:"
+echo 
+echo "curl"
+echo
+echo "Other actions:"
+echo 
+echo "Give the EXECUTION permission to the WTTR.SH file"
+echo
+echo "If you authorize, press ENTER"
+read
