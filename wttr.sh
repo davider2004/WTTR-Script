7201a4b9f1dec29c7f46eac25dbbbf461f81e570
DAVIDE RAMONDETTI <info@ramondettidavide.com>
2017-01-27 16:29:54 +0100

    Create wttr.sh

---
+++ wttr.sh
@@ -0,0 +1,7 @@
+#######################################
+# (c) 2017 RamondettiDavide Softwares #
+#          ramondettidavide.com       #
+#######################################
+
+CITY="Turin" #NAME_OF_YOUR_SITE_IN_ENGLISH
+
